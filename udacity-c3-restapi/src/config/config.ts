93a0f335d211fc71fdeea0e8f5f164ce22520e1d
eyawson <ebow.yawson@gmail.com>
2019-12-11 00:45:29 -0500

    fix: fixed env var names
    Changed environment variable names to match vars stored on local and aws

--- udacity-c3-restapi/src/config/config.ts
+++ udacity-c3-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
-export const config = {
+/* export const config = {
   "dev": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
     "database": process.env.POSTGRESS_DB,
     "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
+    "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_BUCKET
   },
@@ -21,3 +21,28 @@ export const config = {
   }
 
 }
+ */
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+
+  "jwt": {
+    "secret": "HelloWorld"
+  }
+};
